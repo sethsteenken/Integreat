949bad503771744ce98f179759afd2d552dbad74
Seth Steenken <sethsteenken@gmail.com>
2018-08-09 21:08:11 -0400

    Added initial file-based logic - no implementations yet

--- src/Integreat.Core/Models/Executables/FileExecutable.cs
+++ src/Integreat.Core/Models/Executables/FileExecutable.cs
@@ -8,39 +8,35 @@ namespace Integreat.Core
 {
     public abstract class FileExecutable : ExecutableBase
     {
-        //private readonly IFileStorage _fileStorage;
-
         //protected FileExecutable(IProcess process, IFileStorage fileStorage, ExecutableReference reference)
         //    : this(process, fileStorage, reference.Timeout, reference.Parameters, reference.File)
         //{
 
         //}
 
-        //protected FileExecutable(IProcess process, IFileStorage fileStorage, int timeout, ExecutionParameters parameters, string file)
-        //    : base(process, timeout, parameters)
-        //{
-        //    if (string.IsNullOrWhiteSpace(file))
-        //        throw new ArgumentNullException(nameof(file), $"A filename value for File is required to execute {GetType().FullName} executable.");
+        protected FileExecutable(IFileStorage fileStorage, string file)
+        {
+            if (string.IsNullOrWhiteSpace(file))
+                throw new ArgumentNullException(nameof(file), $"A filename value for File is required to execute {GetType().FullName} executable.");
 
-        //    _fileStorage = fileStorage;
-        //    File = file.Trim();
-        //}
+            FileStorage = fileStorage;
+            File = file.Trim();
+        }
 
         protected string File { get; private set; }
-        //protected IFileStorage FileStorage => _fileStorage;
+        protected IFileStorage FileStorage { get; private set; }
 
         public override string Name => $"Type: {GetType().Name}, File: {File}";
 
         protected override void OnExecute(ExecutableContext context)
-        {
-            string filePath = "";// _fileStorage.Join(executablesDirectory, File);
+        { 
+            var file = FileStorage.GetFile(Path.Combine(context.ExecutablesDirectory, File));
+            if (!file.Exists)
+                throw new FileNotFoundException($"Executable file '{file.FullPath}' not found.");
 
-            //if (!_fileStorage.Exists(filePath))
-            //    throw new FileNotFoundException($"Executable file '{filePath}' not found.");
-
-            OnExecute(context, filePath);
+            OnExecute(context, file);
         }
 
-        protected abstract void OnExecute(ExecutableContext context, string filePath);
+        protected abstract void OnExecute(ExecutableContext context, IFile file);
     }
 }
