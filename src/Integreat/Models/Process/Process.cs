3f5ea70682ee6c57dbefc52e57101fffce6dce03
Seth Steenken <sethsteenken@gmail.com>
2018-08-12 17:09:03 -0400

    Added Process item (WIP)

--- src/Integreat/Models/Process/Process.cs
+++ src/Integreat/Models/Process/Process.cs
@@ -1,29 +1,45 @@
-﻿using System;
+﻿using Integreat.Core;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Integreat
 {
-    public class Process : IProcess
+    public sealed class Process : IProcess
     {
-        public Process()
-        {
+        private readonly IReadOnlyList<IExecutable> _executables;
+        private readonly ILogger<Process> _logger;
+        private readonly StringBuilder _resultBuilder;
 
+        public Process(IReadOnlyList<IExecutable> executables, ILogger<Process> logger)
+        {
+            _executables = executables;
+            _logger = logger;
+            Id = Guid.NewGuid();
+            _resultBuilder = new StringBuilder();
         }
 
-        public Guid Id => throw new NotImplementedException();
+        public Guid Id { get; private set; }
 
-        public string Result => throw new NotImplementedException();
+        public string Result => _resultBuilder.ToString();
 
         public void Execute()
         {
-            //if (Executables == null || Executables.Count == 0)
-            //    throw new InvalidOperationException("There are no Executables registered in the Execution Plan.");
+            if (_executables == null || _executables.Count == 0)
+                throw new InvalidOperationException("There are no Executables registered in the Execution Plan.");
 
-            //foreach (var executable in Executables)
-            //{
-            //    executable.Execute(new ExecutionContext(IntegrationDirectory, ExecutablesDirectory));
-            //}
+            foreach (var executable in _executables)
+            {
+                // TODO
+                executable.Execute(new ExecutableContext("integrationDir", "executablesDir", new ExecutableParameters(), 60, Log));
+            }
+        }
+
+        private void Log(string message)
+        {
+            _logger.LogInformation(message);
+            _resultBuilder.AppendLine(LogFormatter.FormatMessage(message));
         }
     }
 }
