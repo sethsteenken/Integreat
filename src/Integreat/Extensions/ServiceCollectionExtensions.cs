06c0d8d9cb890215ba10ee6f13b5295a7a9b022a
Seth Steenken <sethsteenken@gmail.com>
2018-08-26 12:58:23 -0400

    Added initial servicecollection additions

---
+++ src/Integreat/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Integreat
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddIntegreat(this IServiceCollection services)
+        {
+            Guard.IsNotNull(services, nameof(services));
+
+            // TODO - load in integration settings from json file?
+
+            services.AddSingleton<IIntegrationSettings, IntegrationSettings>();
+            services.AddSingleton<IFileStorage, SystemIOFileStorage>();
+            services.AddSingleton<ISerializer, JsonNetSerializer>();
+            services.AddSingleton<IProcessFactory, ProcessFactory>();
+
+            services.AddSingleton<IWatcher, FileWatcher>();
+
+            // TODO - add adapters - how to register?
+
+
+            // TODO - add microsoft logging
+
+            return services;
+        }
+    }
+}
--- src/Integreat/Services/Process/ProcessFactory.cs
+++ src/Integreat/Services/Process/ProcessFactory.cs
@@ -1,27 +1,43 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 
 namespace Integreat
 {
     public class ProcessFactory : IProcessFactory
     {
         private readonly ILoggerFactory _loggerFactory;
-        private readonly IProcessSetup _processSetup;
-        private readonly IProcessExecutor _processExecutor;
+        private readonly IFileStorage _fileStorage;
+        private readonly IIntegrationSettings _settings;
+        private readonly ISerializer _serializer;
+        private readonly IDictionary<string, IProcessExecutableAdapter> _adapters;
 
-        public ProcessFactory(ILoggerFactory loggerFactory, IProcessSetup processSetup, IProcessExecutor processExecutor)
+        public ProcessFactory(ILoggerFactory loggerFactory, 
+            IFileStorage fileStorage, 
+            IIntegrationSettings settings, 
+            ISerializer serializer, 
+            IDictionary<string, IProcessExecutableAdapter> adapters)
         {
             _loggerFactory = loggerFactory;
-            _processSetup = processSetup;
-            _processExecutor = processExecutor;
+            _fileStorage = fileStorage;
+            _settings = settings;
+            _serializer = serializer;
+            _adapters = adapters;
         }
 
         public IProcess Create()
         {
             var processId = Guid.NewGuid();
+
             IProcessLogger processLogger = new ProcessLogger(_loggerFactory.CreateLogger<ProcessLogger>(), processId);
 
-            return new Process(processId, processLogger, _processSetup, _processExecutor);
+            IIntegrationArchiver archiver = new IntegrationArchiver(_fileStorage, processLogger, _settings);
+            IIntegrationFileHandler fileHandler = new IntegrationFileHandler(_fileStorage, processLogger, _settings);
+            IProcessSetup processSetup = new ProcessSetup(archiver, fileHandler);
+            IExecutionPlanFactory executionPlanFactory = new ExecutionPlanFactory(_serializer, fileHandler, _adapters);
+            IProcessExecutor processExecutor = new ProcessExecutor(processLogger, executionPlanFactory);
+
+            return new Process(processId, processLogger, processSetup, processExecutor);
         }
     }
 }
