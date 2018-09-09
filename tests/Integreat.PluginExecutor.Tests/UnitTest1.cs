7b3be556cc39247c9f87487c59baaa1cb87d1a72
Seth Steenken <sethsteenken@gmail.com>
2018-09-09 15:11:57 -0400

    Added some initial test projects. Added under test folder outside of src

---
+++ tests/Integreat.PluginExecutor.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Integreat.PluginExecutor.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
